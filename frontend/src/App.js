7ce6c4d978db4fd1f55eb6bb8f38cd731d782628
Kevin <kevinkwon1995@gmail.com>
2020-04-20 10:48:45 -0700

    Allow users to send custom messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { connect, sendMsg } from "./api";
 import Header from './components/Header/Header'
 import ChatHistory from './components/ChatHistory/ChatHistory'
+import ChatInput from './components/ChatInput/ChatInput'
 
 class App extends React.Component {
   constructor(props) {
@@ -20,9 +21,11 @@ class App extends React.Component {
     });
   }
 
-  send() {
-    console.log("hello");
-    sendMsg("hello");
+  send(event) {
+    if(event.keyCode === 13) {
+      sendMsg(event.target.value);
+      event.target.value = "";
+    }
   }
 
   render() {
@@ -30,6 +33,7 @@ class App extends React.Component {
       <div className="App">
         <Header/>
         <ChatHistory chatHistory={this.state.chatHistory} />
+        <ChatInput send={this.send} />
         <button onClick={this.send}>Hit</button>
       </div>
     )
